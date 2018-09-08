de031a8fded059fae87b529e83d801f69e285c55
HendrikBD <benjamin.danen@gmail.com>
2018-09-08 17:26:08 -0400

    Removing clone commands, vundle will take care of it

--- setup.sh
+++ setup.sh
@@ -1,18 +1,3 @@
-# A simple install script to download and install all necessary files and bundles for vim
+# A simple script to perform setup commands to prepare a vim environment
 
-# Cloning all packages
-
-# Color/appearance
-git clone https://github.com/sjl/badwolf # Pretty cool color scheme
-
-
-git clone https://github.com/scrooloose/nerdtree # Tree file explorer
-git clone https://github.com/ctrlpvim/ctrlp.vim # Fuzzy file searcher
-git clone https://github.com/mbbill/undotree # Visualizes undos as a tree
-git clone https://github.com/ggreer/the_silver_searcher # Code searching similar to ag
-git clone https://github.com/tomtom/tcomment_vim # Tool to allow commentting hotkey for various filetypes
-git clone https://github.com/pangloss/vim-javascript # JavaScript plugin for syntax highlinghting & indentation
-git clone https://github.com/leafgarland/typescript-vim # TypeScript plugin for syntax highlinghting & indentation
-git clone https://github.com/vim-airline/vim-airline # Airline, a simple status bar for vim
-git clone https://github.com/vim-airline/vim-airline-themes # Themes for airline
 
