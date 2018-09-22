b5f7e6ae7fc97a2e0bc7331a2ec2960527040a14
HendrikBD <benjamin.danen@gmail.com>
2018-09-22 14:11:39 -0400

    Adding comment and spacings

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 # A simple script to perform setup commands to prepare a vim environment
 
+# Download or update Vundle package manager for vim
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
-        git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
+  git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
 else
-        git -C ~/.vim/bundle/Vundle.vim pull
+  git -C ~/.vim/bundle/Vundle.vim pull
 fi
 
 vim +PluginInstall +qall
