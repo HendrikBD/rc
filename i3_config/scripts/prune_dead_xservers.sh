c0f98d89c6ac613dfd3717c97e323a36b687747e
HendrikBD <hendrik.danen@gmail.com>
2020-05-09 19:13:53 -0400

    Building a working script to prune closed xephyr servers.

--- i3_config/scripts/prune_dead_xservers.sh
+++ i3_config/scripts/prune_dead_xservers.sh
@@ -1,14 +1,51 @@
 #!/bin/bash
-get_xephyr_servers() {
-  x_servers=`ps e -u username | sed -rn 's/.* DISPLAY=(:[0-9]*).*/\1/p'`
-  echo $x_servers > ~/.test/test_`date +%s`
-}
 
 get_xephyr_windows() {
-  echo test
+  xephyr_windows=`i3-msg -t get_tree | grep "Xephyr" | sed 's/.*Xephyr on ://g' | sed 's/\..*//g'`
+  echo $xephyr_windows
 }
 
-touch /home/bhd/.time/timestamp_`date +%s`
-echo 'hello?'
+get_xephyr_servers() {
 
-get_xephyr_servers
+  x_servers=`ps aux |\
+    grep "Xephyr :.*ac" |\
+    sed -rn 's/.*Xephyr\ ://p' |\
+    sed -rn 's/\ -ac.*//p' |\
+    tr " " "\n" |\
+    sort -nu`
+
+  echo $x_servers
+}
+
+get_headless_xservers() {
+  xephyr_servers=`get_xephyr_servers`
+  xephyr_windows=`get_xephyr_windows`
+  headless_xservers=`echo $xephyr_windows $xephyr_servers | tr " " "\n" | sort | uniq -u`
+  echo $headless_xservers
+}
+
+kill_headless_xephyr_servers() {
+
+  headless_xservers=`get_headless_xservers`
+  echo $headless_xservers
+
+  for i in ${headless_xservers// / }
+  do
+    pid=`get_xephyr_pid $i`
+    echo $pid
+    kill $pid
+  done
+}
+
+get_xephyr_pid() {
+
+  x_server_details=`ps aux |\
+    grep "Xephyr.*$1.*ac" |\
+    grep -v "grep"`
+
+  x_server_pid=`echo $x_server_details | awk -F " " '{print $2}'`
+
+  echo $x_server_pid
+}
+
+kill_headless_xephyr_servers
