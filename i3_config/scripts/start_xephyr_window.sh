ab3abbdf1a4476c5fb2c0f7a63d40930a014a519
HendrikBD <hendrik.danen@gmail.com>
2020-05-09 17:32:42 -0400

    Making start xephyr script start a server.

--- i3_config/scripts/start_xephyr_window.sh
+++ i3_config/scripts/start_xephyr_window.sh
@@ -1,5 +1,27 @@
 get_next_xephyr_session() {
-  echo 'test'
+
+  x_servers=`get_xephyr_servers`
+
+  prev_x_server=`echo $x_servers |\
+    awk -F " " '{print $NF}'`
+
+  next_x_server=`expr $prev_x_server + 1`
+
+  Xephyr :$next_x_server -ac -screen 460x340 &> /dev/null &
 }
 
-Xephyr :2 -ac -screen 460x340 &> /dev/null &
+get_xephyr_servers() {
+
+  x_servers=`ps aux |\
+    grep "Xephyr :.*ac" |\
+    sed -rn 's/.*Xephyr\ ://p' |\
+    sed -rn 's/\ -ac.*//p' |\
+    tr " " "\n" |\
+    sort -nu`
+
+  echo $x_servers
+}
+
+next_xephyr_session=`get_next_xephyr_session`
+
+eval `Xephyr :${next_x_server} -ac -screen 460x340 &> /dev/null &`
