c9f926a132eb91d4a40cb5e946fa9630ac1499b3
HendrikBD <hendrik.danen@gmail.com>
2020-05-10 14:00:38 -0400

    Updating pscircle position + increasing font

--- i3_config/scripts/update_pscircle.sh
+++ i3_config/scripts/update_pscircle.sh
@@ -3,7 +3,7 @@
 while true;
 do
   DISPLAY=:1
-  ps -e -o pid | pscircle --root-pid=1 --collapse-threads=true --max-children=80 --cpulist-center=300:0 --memlist-center=800:0 --output-height=1080 --output-width=3840 --tree-font-size=9 --tree-radius-increment=60 --background-color=000000 --tree-center=-1580:160 --output=/tmp/pscircle_1 && \
+  ps -e -o pid | pscircle --root-pid=1 --collapse-threads=true --max-children=60 --cpulist-center=300:0 --memlist-center=800:0 --output-height=1080 --output-width=3840 --tree-font-size=10 --tree-radius-increment=60 --background-color=000000 --tree-center=-1580:180 --output=/tmp/pscircle_1 && \
   nitrogen --set-zoom-fill /tmp/pscircle_1
   sleep 3
 done
