832012f12f8bf116c384c0abc8cb9442f3706f7a
HendrikBD <hendrik.danen@gmail.com>
2020-05-10 21:58:29 -0400

    Updating init script to use conky scripts from config

--- i3/scripts/i3_init.sh
+++ i3/scripts/i3_init.sh
@@ -3,5 +3,5 @@ xmodmap -e "clear lock" #disable caps lock switch
 xmodmap -e "keysym Caps_Lock = Escape" #set caps_lock as escape
 
 # Conky
-conky -c $HOME/.conky/SimplecOne/conkyrc3 --display=:1 -x -1920 -y 0
-conky -c $HOME/.conky/SimplecOne/conkyrc4 --display=:1 -x -1520 -y 20
+conky -c $HOME/.config/conky/SimplecOne/conkyrc3 --display=:1 -x -1920 -y 0
+conky -c $HOME/.config/conky/SimplecOne/conkyrc4 --display=:1 -x -1520 -y 20
