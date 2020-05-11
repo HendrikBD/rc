c11a7211e09e243a9d52639b317e5113255e76be
HendrikBD <hendrik.danen@gmail.com>
2020-05-10 21:55:47 -0400

    Adding in simple conky widgets, pretty nice looking

---
+++ conky/SimplecOne/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+~/.conky/SimplecOne/weather.sh &
+conky -q -c ~/.conky/SimplecOne/conkyrc1 &
+conky -q -c ~/.conky/SimplecOne/conkyrc2 &
+conky -q -c ~/.conky/SimplecOne/conkyrc3 &
+conky -q -c ~/.conky/SimplecOne/conkyrc4 &
+
+exit
+
+
+conky -q -c ~/.conky/SimplecOne/conkyrc3 -x 10 -y 10 &
